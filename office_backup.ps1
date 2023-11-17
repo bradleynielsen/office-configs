d4a98ce6a0fdb2b628bb11870daabf62da2a830f
Nielsen <Brad.Nielsen@serco-na.com>
2023-11-17 12:42:37 -0600

    sync script commit

--- office_backup.ps1
+++ office_backup.ps1
@@ -1,7 +1,11 @@
 $macroPath =  "$env:APPDATA\Microsoft\Excel\XLSTART"
-#$savePath = "c:"+$env:HOMEPATH+'\OneDrive - Serco\Documents\backup\office configs\xl'
-$savePath = $env:OneDrive+'\Documents\backup\office configs\xl'
+$savePath = $env:OneDrive+'\Documents\backup\office-configs\xl'
 $buItems = gci $macroPath
 foreach ($item in $buItems){  
     $item | Copy-Item -Destination "$savePath\backup.PERSONAL.XLSB"
 }
+
+
+
+
+#$savePath = "c:"+$env:HOMEPATH+'\OneDrive - Serco\Documents\backup\office-configs\xl'
---
+++ sync.sh
@@ -0,0 +1,4 @@
+powershell ./office_backup.ps1
+git add .
+git commit -m "sync script commit"
+git push
